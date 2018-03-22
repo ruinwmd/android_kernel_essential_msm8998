862b93dfa5adc2800f9b7995f7c9cdeb5b01e0cc
Nathan Chancellor <natechancellor@gmail.com>
2018-03-22 06:01:49 -0700

    drivers: staging: qcacld-3.0: Fix unused function warning
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -12173,6 +12173,7 @@ dev_alloc_err:
 	return -ENODEV;
 }
 
+#ifdef MODULE
 static void wlan_hdd_state_ctrl_param_destroy(void)
 {
 	cdev_del(&wlan_hdd_state_cdev);
@@ -12182,6 +12183,7 @@ static void wlan_hdd_state_ctrl_param_destroy(void)
 
 	pr_info("Device node unregistered");
 }
+#endif
 
 /**
  * __hdd_module_init - Module init helper
